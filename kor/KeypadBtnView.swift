64a1cdd0a74787de72a9e12720dcbaec76eb1a24
ian <ian.park@koreapolyschool.com>
2019-01-03 10:44:12 +0900

    first commit

---
+++ kor/KeypadBtnView.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  kor
+//
+//  Created by 박일호 on 31/12/2018.
+//  Copyright © 2018 박일호. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class KeypadBtnView: UIView {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
